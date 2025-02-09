a2e1b022d1aa4122b696e7be344f1a35542b5e12
Liedson Oliveira <liedsonwork@hotmail.com>
2025-02-09 11:35:27 -0300

    feat: adicionando loadings em alguns lugares

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,10 +6,13 @@ import './Login.css';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // Estado para controle de loading
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    setIsLoading(true); // Iniciar o loading
+
     try {
       const response = await axios.post('http://localhost:8081/auth/login', { username, password });
       localStorage.setItem('token', response.data);
@@ -18,6 +21,8 @@ const Login = () => {
       navigate('/dashboard');
     } catch (error) {
       alert('Login falhou.');
+    } finally {
+      setIsLoading(false); // Finalizar o loading
     }
   };
 
@@ -29,21 +34,28 @@ const Login = () => {
       <div className="login-right">
         <div className="login-container">
           <h2>Login</h2>
-          <form onSubmit={handleLogin}>
-            <input
-              type="text"
-              placeholder="Usuário"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Senha"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Entrar</button>
-          </form>
+          {isLoading ? (
+            <div className="loading-message">
+              <p>Render está carregando, aguarde um momento...</p>
+              <div className="spinner"></div> {/* Adiciona o spinner */}
+            </div>
+          ) : (
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                placeholder="Usuário"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Entrar</button>
+            </form>
+          )}
         </div>
       </div>
     </div>
